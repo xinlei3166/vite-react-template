1137a3c1cbc68600b41f500e48f3e582e8614d56
君惜 <xinlei3166@126.com>
2024-08-08 15:31:34 +0800

    feat: code optimization

--- apps/react-app/src/main.tsx
+++ apps/react-app/src/main.tsx
@@ -9,9 +9,10 @@ import 'virtual:uno.css'
 import('./mock')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 )
 
 // import * as serviceWorker from './serviceWorker'
