d9694e28fb4df33609d348b847d1c78ab5224015
君惜 <xinlei3166@126.com>
2026-03-15 23:56:12 +0800

    feat: manage folder

---
+++ apps/next-app/next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export', // 输出单页面应用 (SPA)
+  distDir: './dist', // 将构建输出目录更改为 `./dist/`
+  compiler: {
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
+  transpilePackages: ["lodash-es"],
+};
+
+export default nextConfig;
